ea82b239188c799e8305a1260a35c727ed783bee
John Dickerson <john.charles.dickerson@gmail.com>
2023-11-06 09:19:46 +0300

    Implementing LinkXTwitter.tsx

--- src/components/link-platform/x-twitter/LinkXTwitter.tsx
+++ src/components/link-platform/x-twitter/LinkXTwitter.tsx
@@ -1,9 +1,15 @@
 import CaptureXHandle from './children/CaptureXHandle';
+import { Link } from "react-router-dom";
 
 export default function LinkXTwitter() {
 
   return (
     <div>
+      <div style={{ textAlign: 'right' }}>
+        <Link to={'/'}>
+          Back
+        </Link>
+      </div>
       <CaptureXHandle />
     </div>
   );
